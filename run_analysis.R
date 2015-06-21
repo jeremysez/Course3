9b7525af7813aa49ef7c5ab595da30a6d6b03c92
Jeremy Sims <jeremy.sims@easyjet.com>
2015-06-21 16:32:00 +0100

    3rd commit

---
+++ run_analysis.R
@@ -0,0 +1,51 @@
+
+# 1. Download and unzip file to working directory
+fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+download.file(fileUrl, destfile = "Dataset.zip", method = "curl")
+unzip("Dataset.zip")
+
+# 2. Load any packages needed
+library(data.table)
+
+
+# 3. Read test and training files
+testData <- read.table("./UCI HAR Dataset/test/X_test.txt",header=FALSE)
+testData_act <- read.table("./UCI HAR Dataset/test/y_test.txt",header=FALSE)
+testData_sub <- read.table("./UCI HAR Dataset/test/subject_test.txt",header=FALSE)
+trainData <- read.table("./UCI HAR Dataset/train/X_train.txt",header=FALSE)
+trainData_act <- read.table("./UCI HAR Dataset/train/y_train.txt",header=FALSE)
+trainData_sub <- read.table("./UCI HAR Dataset/train/subject_train.txt",header=FALSE)
+
+# 4. Get the descriptive names for the activities
+activities <- read.table("./UCI HAR Dataset/activity_labels.txt",header=FALSE,colClasses="character")
+testData_act$V1 <- factor(testData_act$V1,levels=activities$V1,labels=activities$V2)
+trainData_act$V1 <- factor(trainData_act$V1,levels=activities$V1,labels=activities$V2)
+
+# 5. Label the data set with descriptive names
+features <- read.table("./UCI HAR Dataset/features.txt",header=FALSE,colClasses="character")
+colnames(testData)<-features$V2
+colnames(trainData)<-features$V2
+colnames(testData_act)<-c("Activity")
+colnames(trainData_act)<-c("Activity")
+colnames(testData_sub)<-c("Subject")
+colnames(trainData_sub)<-c("Subject")
+
+# 6. Bind test and training sets into one data set
+testData<-cbind(testData,testData_act)
+testData<-cbind(testData,testData_sub)
+trainData<-cbind(trainData,trainData_act)
+trainData<-cbind(trainData,trainData_sub)
+bigData<-rbind(testData,trainData)
+
+# 7. Extract only the measurements on the mean and standard deviation. Sorry, have tried, but can not get it to work!
+# all_names<- names(all_full)
+# name_condition<-"mean()|std()"
+# good<-grepl(name_condition,all_names)
+# good_names<-all_names[good]
+# good_names<-c("Activity","Subject",good_names)
+# extract <- all_full[,good_names,drop=FALSE]
+
+# 8. Create tidy data set with the average of each variable for each activity and each subject.
+DT <- data.table(bigData)
+tidy<-DT[,lapply(.SD,mean),by="Activity,Subject"]
+write.table(tidy,file="tidy.txt",sep=",",row.names = FALSE)
